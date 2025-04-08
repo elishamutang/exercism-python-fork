18eb937cfbb837db3f30261b26e9a905c6ae9677
elishamutang <elishamutang@outlook.com>
2025-04-08 15:01:23 +0800

    Initial solution.

--- exercises/practice/binary-search-tree/binary_search_tree.py
+++ exercises/practice/binary-search-tree/binary_search_tree.py
@@ -1,8 +1,8 @@
 class TreeNode:
     def __init__(self, data, left=None, right=None):
-        self.data = None
-        self.left = None
-        self.right = None
+        self.data = data
+        self.left = left
+        self.right = right
 
     def __str__(self):
         return f'TreeNode(data={self.data}, left={self.left}, right={self.right})'
@@ -10,10 +10,23 @@ class TreeNode:
 
 class BinarySearchTree:
     def __init__(self, tree_data):
-        pass
+        self._tree_data = tree_data
 
     def data(self):
-        pass
+        if len(self._tree_data) == 1:
+            return TreeNode(self._tree_data[0])
+
+        root = self._tree_data[0]
+        tree = TreeNode(root)
+
+        for num in self._tree_data[1:]:
+            if num <= root:
+                tree.left = TreeNode(num)
+            else:
+                tree.right = TreeNode(num)
+
+        print(tree)
+        return tree
 
     def sorted_data(self):
         pass
